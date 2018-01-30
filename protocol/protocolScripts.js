07aa7428ab12d0e77e94bbe37b5a97bd75ac9019
dthankins <34296958+dthankins@users.noreply.github.com>
2018-01-29 23:01:15 -0600

    Create protocolScripts.js

---
+++ protocol/protocolScripts.js
@@ -0,0 +1,75 @@
+function femaleCheck() {
+    var checkBox = document.getElementById("female-check");
+    var text = document.getElementById("preg-questions");
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+}
+
+
+function ucgNotDone() {
+    var checkBox = document.getElementById("ucg-check");
+    var text = document.getElementById("reason-questions");
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+}
+
+function sameExam() {
+    var checkBox = document.getElementById("same-exam-check");
+    var text = document.getElementById("suggested-questions");
+    if (checkBox.checked == false){
+        text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+}
+
+
+function JSshowOlderGFR() {
+    var checkBox = document.getElementById("eGFR-check");
+    var text = document.getElementById("showOlderGFR");
+    if (checkBox.checked == false){
+        text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+}
+
+/*
+
+
+
+window.onload = function () {
+
+	// event listener 
+	document.getElementsByName("eGFR")[0].addEventListener('change', showOlderGFR());
+
+	//function 
+	function showOlderGFR(){
+
+	   	var eGFR = document.getElementByName("eGFR").value;
+	    var text = document.getElementById("showOlderGFR");
+	    if (eGFR < 60){
+	        text.style.display = "block";
+	    } else {
+	       text.style.display = "none";
+	    }
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
+*/
