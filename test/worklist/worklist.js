2470e83adc86e6e8c9c7ee3fc8077845822fec30
dthankins <34296958+dthankins@users.noreply.github.com>
2018-03-18 09:00:23 -0500

    added refresh

--- test/worklist/worklist.js
+++ test/worklist/worklist.js
@@ -1,4 +1,14 @@
 
+/*
+
+I need to change to using appendchild and remove child then 
+add a field for prvious list to remove or always have it only on one list by auto removing all others.
+would still have the problem of refreshing all lists every time.  Not sure if this is really a problem.
+
+*/
+
+
+
 function initializeFirebase() {
 
 
@@ -14,11 +24,7 @@ function initializeFirebase() {
       
     firebase.initializeApp(config);
 
-    //test to see it is working  it never acts like it is working   
-    // console.log(firebase);
 
-    // Reference to the protocols object in your Firebase database
-    // var newProtocols = firebase.database().ref("newProtocols");
 }
 
 
@@ -35,31 +41,10 @@ function readData() {
 
 }
 
-/*
- if (document.getElementById('office') != null && realdata[keys[i]].status === 1){
-            office(realdata[keys[i]].firstname);
-        }
-        if (realdata[keys[i]].status === 2){
-            officeDone(realdata[keys[i]].firstname);
-        }
-        if (realdata[keys[i]].status === 3){
-            tech(realdata[keys[i]].firstname);
-        }
-        if (realdata[keys[i]].status === 4){
-            techDone(realdata[keys[i]].firstname);
-        }
-        if (realdata[keys[i]].status === 5){
-            rad(realdata[keys[i]].firstname);
-        }
-        if (document.getElementById('radDone') != null && realdata[keys[i]].status === 6){
-            radDone(realdata[keys[i]].firstname);
-        }
-       
-        if (realdata[keys[i]].status === 7){
-            printDone(realdata[keys[i]].firstname);
-*/
 
 function outputData(data) {
+
+    refreshLists();
    
     realdata = data.val(); 
     var keys = Object.keys(data.val());
@@ -88,6 +73,16 @@ function outputData(data) {
         }
     }
        
+}
+
+function refreshLists(){
+    document.getElementById('office').innerHTML = "";
+    document.getElementById('officeDone').innerHTML = "";
+    document.getElementById('tech').innerHTML = "";
+    document.getElementById('techDone').innerHTML = "";
+    document.getElementById('rad').innerHTML = "";
+    document.getElementById('radDone').innerHTML = "";
+    document.getElementById('printDone').innerHTML = "";
 }
 
 function office (name){
