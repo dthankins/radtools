5d575c796fa664349f641c4d8fa13c4681f4ba3d
dthankins <34296958+dthankins@users.noreply.github.com>
2019-11-29 14:33:44 -0600

    Add files via upload

---
+++ ovary/ovary.js
@@ -0,0 +1,39 @@
+// Global Var Section
+// var angle;
+
+
+console.log("global var section");
+
+
+function setup() {
+    console.log("setup section");
+    //createCanvas(windowWidth, windowHeight);
+    //angleMode(DEGREES);
+    //rectMode(CENTER);
+    //frameRate(10);
+
+    //screenWidth=windowWidth;
+    //screenHeight=windowHeight;
+
+
+
+
+  
+} // end setup
+
+
+function draw() {
+
+    console.log("draw section");
+    //background(0);
+    //fill(255, 0, 0);
+    //ellipse(111,111,44,44);
+
+
+    
+
+} // end main draw function
+
+// Other functions go below here.
+
+
